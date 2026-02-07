0181aafc7604647fcce3b769cca774a402d99585
Tshegofatso Mkhabela <161270475+TshegofatsoMkhabela@users.noreply.github.com>
2026-02-07 17:14:45 +0200

    Revert "feat: initial backend setup with .NET 9, Docker and PostgreSQL"

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,32 +1,3 @@
-using Backend.Data; 
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// --- CONNECT DATABASE ---
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-=======
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
